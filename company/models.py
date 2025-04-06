b561d9b2c21f6a0f8a588a86f0c8530eba5f4507
atAddan <odyner@mail.ru>
2025-04-06 21:11:54 +0300

    new

--- company/models.py
+++ company/models.py
@@ -1,13 +1,4 @@
-"""Module has a function that prints the name of my site"""
-
 from django.db import models
-
-
-def print_site_name():
-    site = "bobbyhadz.com"
-
-    print(site)
-
 
 class Filial(models.Model):
     name = models.CharField(max_length=44)
