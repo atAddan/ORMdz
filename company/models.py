d452e4aa1842146f94ef58602b6010757a7afd21
atAddan <odyner@mail.ru>
2025-04-06 20:50:43 +0300

    ww

--- company/models.py
+++ company/models.py
@@ -4,28 +4,32 @@ from django.db import models
 class Filial(models.Model):
     name = models.CharField(max_length=44)
     address = models.CharField(max_length=255)
-    
+
+
 class Otdel(models.Model):
     name = models.CharField(max_length=255)
     flor = models.IntegerField()
     filial = models.ForeignKey(
-        'company.Filial',
+        "company.Filial",
         null=True,
-        related_name='Otdels',
+        related_name="Otdels",
         on_delete=models.SET_NULL,
-        )
-    
+    )
+
+
 class Sotr(models.Model):
     full_name = models.CharField(max_length=255)
     doljnost = models.CharField(max_length=255)
-    fone = models.CharField(max_length=20,blank=True, null=True)
+    fone = models.CharField(max_length=20, blank=True, null=True)
     data = models.DateTimeField(blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
-    
+
     otdel = models.ForeignKey(
-        'company.Otdel',
+        "company.Otdel",
         null=True,
-        related_name='Sotrs',
+        related_name="Sotrs",
         on_delete=models.CASCADE,
-        )
+    )
+
+
 # Create your models here.
